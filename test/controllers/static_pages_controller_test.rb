29088e91bf75b762c5b86a3144ce36434084729e
juandalobos <juan.villalobos@somosinternet.co>
2025-05-13 14:41:55 -0400

    Finish user signup form with flash message

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,7 +5,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get root_path
     assert_response :success
     assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
-    assert_select "h1", "Sample App"
+    assert_select "h1", "Welcome to the Sample App"
   end
 
   test "should get help" do
