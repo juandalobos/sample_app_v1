a326bb22cfb37f3defc7ad39216d2a9d05b5da89
juandalobos <juan.villalobos@somosinternet.co>
2025-06-02 21:59:59 -0500

    Fix redirect after user deletion

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,11 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
 
   def index
-    @users = User.where(activated: true).paginate(page: params[:page])
+    @users = User.paginate(page: params[:page])
   end
 
   def show
     @user = User.find(params[:id])
-    redirect_to(root_url) and return unless @user.activated?
   end
 
   def new
@@ -19,37 +18,35 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      @user.send_activation_email
+      UserMailer.account_activation(@user).deliver_now
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
     else
-      render 'new', status: :unprocessable_entity
+      render 'new'
     end
   end
 
   def edit
-    # @user ya está definido en correct_user
+    # @user is set by correct_user
   end
 
   def update
-    # @user ya está definido en correct_user
     if @user.update(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
     else
-      render 'edit', status: :unprocessable_entity
+      render 'edit'
     end
   end
 
   def destroy
     user = User.find(params[:id])
-    if user != current_user
-      user.destroy
+    if user.destroy
       flash[:success] = "User deleted"
     else
-      flash[:danger] = "You can't delete yourself"
+      flash[:danger] = "User could not be deleted"
     end
-    redirect_to users_url, status: :see_other
+    redirect_to users_url
   end
 
   private
@@ -57,6 +54,8 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+
+    # Before filters
 
     def logged_in_user
       unless logged_in?
@@ -68,10 +67,10 @@ class UsersController < ApplicationController
 
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url, status: :see_other) unless current_user?(@user)
+      redirect_to(root_url) unless current_user?(@user)
     end
 
     def admin_user
-      redirect_to(root_url, status: :see_other) unless current_user.admin?
+      redirect_to(root_url) unless current_user.admin?
     end
 end
