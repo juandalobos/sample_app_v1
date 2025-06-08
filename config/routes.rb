6a4b30ffecbacdd641ef0dae32209343d3228fdf
juandalobos <juan.villalobos@somosinternet.co>
2025-06-08 18:11:05 -0500

    Add Micropost resource: routes, controller, model, views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get "password_resets/new"
-  get "password_resets/edit"
   root   'static_pages#home'
   get    '/help',    to: 'static_pages#help'
   get    '/about',   to: 'static_pages#about'
@@ -12,6 +10,7 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
 
   resources :users
+  resources :microposts, only: [:create, :destroy]        # ← AQUI!
   resources :account_activations, only: [:edit]
-  resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
