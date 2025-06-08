31d0165741debd8dcdbffce606940c6b0a8aabb5
juandalobos <juan.villalobos@somosinternet.co>
2025-06-08 17:44:20 -0500

    Fix gravatar_for and feed rendering

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,17 +1,12 @@
 class StaticPagesController < ApplicationController
   def home
     if logged_in?
-      @micropost = current_user.microposts.build
-      @feed_items = current_user.microposts.order(created_at: :desc).paginate(page: params[:page])
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
     end
   end
 
-  def help
-  end
-
-  def about
-  end
-
-  def contact
-  end
+  def help; end
+  def about; end
+  def contact; end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,15 +1,8 @@
 module UsersHelper
-
-  # Gravatar personalizado para el usuario
   def gravatar_for(user, options = {})
-    size    = options[:size] || 80
-    alt     = options[:alt] || user.name
-    classes = options[:class]
-
-    email_hash = Digest::MD5.hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{email_hash}?s=#{size}"
-
-    image_tag(gravatar_url, alt: alt, class: classes)
+    size = options[:size] || 80
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: options[:class])
   end
-
 end
