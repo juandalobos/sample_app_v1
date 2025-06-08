f9ca630adc67bafed48359f67b9cd2dfbd4ece61
juandalobos <juan.villalobos@somosinternet.co>
2025-06-08 17:10:16 -0500

    Allow class option in gravatar_for helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,13 @@
 module UsersHelper
-  # Devuelve la URL del avatar de Gravatar, no una etiqueta <img>
-  def gravatar_for(user, size: 80)
-    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-    "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+
+  # Devuelve una imagen Gravatar para el usuario especificado con opciones personalizables.
+  def gravatar_for(user, size: 80, alt: nil, class: nil)
+    email_hash = Digest::MD5.hexdigest(user.email.downcase)
+    alt_text = alt || user.name
+    classes = class
+
+    gravatar_url = "https://secure.gravatar.com/avatar/#{email_hash}?s=#{size}"
+    image_tag(gravatar_url, alt: alt_text, class: classes)
   end
+
 end
