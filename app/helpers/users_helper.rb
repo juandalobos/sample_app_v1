1a3b39c4e1918b8d0ca69e6848ec1a510d9b9c61
juandalobos <juan.villalobos@somosinternet.co>
2025-06-08 17:13:14 -0500

    Fix gravatar_for compatibility for Heroku/Ruby 3.0+

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,13 +1,15 @@
 module UsersHelper
 
-  # Devuelve una imagen Gravatar para el usuario especificado con opciones personalizables.
-  def gravatar_for(user, size: 80, alt: nil, class: nil)
-    email_hash = Digest::MD5.hexdigest(user.email.downcase)
-    alt_text = alt || user.name
-    classes = class
+  # Gravatar personalizado para el usuario
+  def gravatar_for(user, options = {})
+    size    = options[:size] || 80
+    alt     = options[:alt] || user.name
+    classes = options[:class]
 
+    email_hash = Digest::MD5.hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{email_hash}?s=#{size}"
-    image_tag(gravatar_url, alt: alt_text, class: classes)
+
+    image_tag(gravatar_url, alt: alt, class: classes)
   end
 
 end
