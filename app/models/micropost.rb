0473fb5101d504e9bd988d701f04f640be170bcf
juandalobos <juan.villalobos@somosinternet.co>
2025-06-08 17:52:34 -0500

    Add user feed method

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,8 @@
+# app/models/micropost.rb
 class Micropost < ApplicationRecord
   belongs_to :user
-  validates :content, presence: true, length: { maximum: 140 }
   default_scope -> { order(created_at: :desc) }
+
+  validates :user_id, presence: true
+  validates :content, presence: true, length: { maximum: 140 }
 end
