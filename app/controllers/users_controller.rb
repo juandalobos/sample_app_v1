72056a02e1dba3f701af2eeb10105d002bab20c9
juandalobos <juan.villalobos@somosinternet.co>
2025-06-08 19:10:12 -0500

    Refactor: remove duplicate logged_in_user from UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,6 +56,16 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
+    # Before filters
+
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
     def correct_user
       @user = User.find(params[:id])
       unless current_user?(@user)
