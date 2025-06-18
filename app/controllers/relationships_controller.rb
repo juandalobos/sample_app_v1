87bc53b0e8a890b34ee31a59292eb2cb1e72e44e
juandalobos <juan.villalobos@somosinternet.co>
2025-06-17 20:01:02 -0500

    Corrige RelationshipsController para usar params[:relationship][:followed_id]

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,15 +2,15 @@ class RelationshipsController < ApplicationController
   before_action :logged_in_user
 
   def create
-    user = User.find(params[:followed_id])
-    current_user.follow(user)
-    redirect_to user
+    @user = User.find(params[:relationship][:followed_id])
+    current_user.follow(@user)
+    redirect_to @user
   end
 
   def destroy
     relationship = Relationship.find(params[:id])
-    user = relationship.followed
-    current_user.unfollow(user)
-    redirect_to user
+    @user        = relationship.followed
+    relationship.destroy
+    redirect_to @user
   end
 end
